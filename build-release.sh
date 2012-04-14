a032d5c64311f1c47b09729fea66dc043a934aaa
root <root@h-02.atl.ln.chatmongers.net>
2012-04-14 20:10:01 +0000

    Backup and restore mnesia directory when upgrading surrogate.

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+tarfile=Mnesia-`date "+%Y%m%d%H%M%S"`.tar
+if [ -d rel/surrogate ]; then
+	tar cf $tarfile rel/surrogate/Mnesia*
+
+	echo Backup mnesia to $tarfile
+	tar tf $tarfile
+fi
+
 pushd extras
 ./build-exmpp.sh
 popd
@@ -11,3 +19,8 @@ popd
 cp -r ebin include deps/chatmonger/
 
 ./rebar generate force=1
+
+if [ -f $tarfile ]; then
+	tar xf $tarfile
+fi
+
